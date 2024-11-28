790918ae632ba34130228eb3f3d0748863480ed0
sam zarila <bed-com-03-20@unima.ac.mw>
2024-11-28 14:42:11 +0200

    table not found fix
    fixing useraccount table not found

--- src/useraccounts/useraccounts.service.ts
+++ src/useraccounts/useraccounts.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Useraccount } from './entities/useraccount.entity';
+import { farmersdirectuseraccount } from './entities/useraccount.entity';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(Useraccount)
-    private userRepository: Repository<Useraccount>,
+    @InjectRepository(farmersdirectuseraccount)
+    private userRepository: Repository<farmersdirectuseraccount>,
   ) {}
 
   async register(username: string, password: string): Promise<string> {
